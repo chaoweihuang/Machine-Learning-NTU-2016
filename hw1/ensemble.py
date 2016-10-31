604fa4cda102bf7268c6e4d532f85d19ebc04bc8
whsunset <no.12586269025@gmail.com>
2016-10-31 22:40:56 +0800

    modify ensemble.py

--- hw1/ensemble.py
+++ hw1/ensemble.py
@@ -3,5 +3,5 @@ from utility import read_sub_csv, write_submit_file
 
 import numpy as np
 
-l = [np.array(read_sub_csv(path.join('nn_sub', f))) for f in listdir('nn_sub')]
+l = [np.array(read_sub_csv(path.join('nn_sub', f))) for f in listdir('nn_sub') if '.csv' in f]
 write_submit_file(np.mean(l, axis=0), 'kaggle_best.csv')
